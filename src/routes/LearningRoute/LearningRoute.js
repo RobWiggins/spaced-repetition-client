4e513cc61154dcd13ae554836843093c8e4362dc
Robert Wiggins <wigginsro11@gmail.com>
2019-08-07 17:13:51 -0400

    wrote half of learning component, non func.

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
+import TokenService from '../../services/token-service.js'
+import LanguageService from '../../services/language-api-service'
+import LanguageContext from '../../contexts/LanguageContext';
+import config from '../../config.js'
+
 
 class LearningRoute extends Component {
+  // only change context, not change state 
+  state = {
+    currHead: {},
+  }
+
+  static contextType = LanguageContext;
+
+  componentDidMount() {
+    LanguageService.getHead()
+      .then(data => {
+        if (!data) {
+          console.error(data);
+          throw new Error('Oh no! Something went wrong with getting next word.')
+        }
+        console.log(data);
+        this.setState({currHead: data});
+      })
+      .catch(e => {
+        console.error(e);
+      })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let guess = e.target.guessForm.value;
+    console.log(guess);
+  }
+
   render() {
+    // console.log(this.context.words);
+    // THIS IS WRONG - CHANGE
     return (
       <section>
-        implement and style me
+        <h2>Translate the Word:</h2>
+        <h3>{this.state.currHead.nextWord}</h3>
+        <form htmlFor='guessForm'
+          id="submit-form"
+          onSubmit={e => {
+            e.preventDefault();
+            LanguageService.submitGuess(e.target.guessForm.value);
+          }}
+         >
+          <input type="text" name='guessForm' id="guessForm" required></input>
+          <button>Submit Guess</button>
+        </form>
       </section>
     );
   }
--- src/services/language-api-service.js
+++ src/services/language-api-service.js
@@ -15,6 +15,37 @@ const LanguageApiService = {
           ? res.json().then(e => Promise.reject(e))
           : res.json()
       )
+  },
+  getHead() {
+    console.log('Getting words');
+    return fetch(`${config.API_ENDPOINT}/language/head`, {
+      method: 'GET',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  submitGuess(guess) {
+    console.log('fetching guess: ', guess)
+    let body = JSON.stringify({ 'guess': guess });
+
+    return fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: body,
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
   }
 }
 
