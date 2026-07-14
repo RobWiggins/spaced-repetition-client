7db4aad45ec5e41517a5c45ce357df8d65721c15
robert wiggins <wigginsro11@gmail.com>
2026-07-14 14:02:46 -0400

    made learning container and login container semi-transparent, temp updated config destination

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'https://spaced-rep-server1.herokuapp.com',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  // API_ENDPOINT: 'https://spaced-rep-server1.herokuapp.com',
+  // TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: 'http://localhost:8000', // TODO fix
+  TOKEN_KEY: 'spaced-repetition-auth-token' // TODO value??
 }
