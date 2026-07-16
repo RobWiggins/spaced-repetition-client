dede2d14c7f6c6a7f671290546e9a2cf75f00778
robert wiggins <wigginsro11@gmail.com>
2026-07-16 14:01:44 -0400

    update config endpoint to heroku server API endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // API_ENDPOINT: 'https://spaced-rep-server1.herokuapp.com',
   // TOKEN_KEY: 'blogful-client-auth-token',
-  API_ENDPOINT: 'http://localhost:8000', // TODO fix
+  API_ENDPOINT: 'https://spaced-repetition-d7eebc4adf38.herokuapp.com', // TODO fix
   TOKEN_KEY: 'spaced-repetition-auth-token' // TODO value??
 }
